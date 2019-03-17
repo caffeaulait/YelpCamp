4c0fa7473542d7209023061d0e565596b1c90e42
Yang <yang.youngsoul@gmail.com>
2019-03-17 04:19:22 +0000

    UI Change

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var commentsRoutes = require("./routes/comments")
 var campgroundRoutes = require("./routes/campgrounds")
 var indexRoutes = require("./routes/index")
 var methodOverride = require("method-override")
+var flash = require("connect-flash")
 
 
 // seedDB()
@@ -21,30 +22,26 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser:true})
 
 
 app.set("view engine","ejs")
-
 app.use(bodyParser.urlencoded({extended:true}))
-
 app.use(express.static(__dirname+"/public"))
-
 app.use(methodOverride("_method"))
-
+app.use(flash())
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave:false,
     saveUninitialized: false
 }))
-
 app.use(passport.initialize())
 app.use(passport.session())
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
-
 app.use(function(req,res,next){
     res.locals.currentUser = req.user
+    res.locals.error = req.flash("error")
+    res.locals.success = req.flash("success")
     next()
 })
-
 app.use("/",indexRoutes)
 app.use("/campgrounds/:id/comments",commentsRoutes)
 app.use("/campgrounds",campgroundRoutes)
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var router = express.Router()
 var Campground = require("../models/campground")
+var middleware = require("../middleware")
 
 router.get("/",function(req,res){
     Campground.find({},function(err,allcampgrounds){
@@ -12,7 +13,7 @@ router.get("/",function(req,res){
     })
 })
 
-router.post("/",isLoggedIn,function(req,res){
+router.post("/",middleware.isLoggedIn,function(req,res){
     var name = req.body.name
     var image = req.body.image
     var description = req.body.description
@@ -30,7 +31,7 @@ router.post("/",isLoggedIn,function(req,res){
     })
 })
 
-router.get("/new",isLoggedIn,function(req, res) {
+router.get("/new",middleware.isLoggedIn,function(req, res) {
     res.render("campgrounds/new")
 })
 
@@ -47,7 +48,7 @@ router.get("/:id",function(req, res) {
 })
 
 //edit campground route
-router.get("/:id/edit",checkCampgroundOwnership,function(req, res) {
+router.get("/:id/edit",middleware.checkCampgroundOwnership,function(req, res) {
         Campground.findById(req.params.id,function(foundCampground){ 
                 res.render("campgrounds/edit",{campground:foundCampground})
         })
@@ -56,9 +57,10 @@ router.get("/:id/edit",checkCampgroundOwnership,function(req, res) {
 
 
 //update campground route
-router.put("/:id",function(req,res){
+router.put("/:id",middleware.checkCampgroundOwnership,function(req,res){
     Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCampground){
         if(err){
+            
             res.redirect("/campgrounds")
         }else{
             res.redirect(("/campgrounds/"+req.params.id))
@@ -67,7 +69,7 @@ router.put("/:id",function(req,res){
 })
 
 //destroy campground route
-router.delete("/:id",function(req,res){
+router.delete("/:id",middleware.checkCampgroundOwnership,function(req,res){
     Campground.findByIdAndRemove(req.params.id,function(err){
         if(err){
             res.redirect("/campgrounds")
@@ -81,33 +83,6 @@ router.delete("/:id",function(req,res){
 router.delete("/:comment_id",function(req,res){
     
 })
-
-
-//middleware
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    res.redirect("/login")
-}
-
-function checkCampgroundOwnership(req,res,next){
-        if(req.isAuthenticated()){
-            Campground.findById(req.params.id,function(err,foundCampground){
-            if(err){
-                res.redirect("back")
-            }else{
-                if(foundCampground.author.id.equals(req.user._id)){
-                    next()
-                }else{
-                    res.redirect("back")
-                }
-                }
-            })
-        }else{
-            res.redirect("back")
-        }
-}
 
 
 module.exports = router
